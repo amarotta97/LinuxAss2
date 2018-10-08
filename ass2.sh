bd028c5854325c6f1779f1c459f0f22e4b6627f5
amarotta97 <a.marotta97@gmail.com>
2018-10-08 16:08:55 +1100

    Requirement 3, LED manipulation menu added

--- ass2.sh
+++ ass2.sh
@@ -11,4 +11,58 @@ for files in * Quit; do
         COUNTER=$(expr $COUNTER + 1)
         echo "${COUNTER}. ${files}";
 done
- 
+
+# Function to return the menu of selected LED.
+function secondmenu(){
+        echo "===================="
+        echo "1) turn on"
+        echo "2) turn off"
+        echo "3) associate with a system event"
+        echo "4) associate with the performance of a process"
+        echo "5) stop association with a process' performance"
+        echo "6) quit to main menu"
+        echo "Please enter a number (1-6) for your choice: "
+        echo
+
+        secondcase
+}
+
+echo "Please enter a number (1-6) for the led to configure or quit: "
+echo
+read NUM
+
+# Case statement to select which LED user will be using.
+        case $NUM in
+                1)
+                        cd /sys/class/leds/input0::capslock
+                        printf '%q\n' "${PWD##*/}"
+                        secondmenu
+                        ;;
+                2)
+                        cd /sys/class/leds/input0::numlock
+                        printf '%q\n' "${PWD##*/}"
+                        secondmenu
+                        ;;
+                3)
+                        cd /sys/class/leds/input0::scrolllock
+                        printf '%q\n' "${PWD##*/}"
+                        secondmenu
+                        ;;
+                4)
+                        cd /sys/class/leds/led0
+                        printf '%q\n' "${PWD##*/}"
+                        secondmenu
+                        ;;
+                5)
+                        cd /sys/class/leds/led1
+                        printf '%q\n' "${PWD##*/}"
+                        secondmenu
+                        ;;
+                6)
+                        ;;
+                *)
+                        echo "Invalid option!"
+                        ;;
+
+        esac
+
